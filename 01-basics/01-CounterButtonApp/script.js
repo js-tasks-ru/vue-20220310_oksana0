3c3877be30e4e47fc7678725b5cc7648ceed7bbb
Xenia Freeman <oksanazenkova515@gmail.com>
2022-04-08 01:58:34 +0300

    1.1 1.2 1.3 1.4

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,20 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const Root = defineComponent({
+  name: 'CounterButton',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increase() {
+      this.counter++;
+    },
+  },
+});
+
+const app = createApp(Root);
+const vm = app.mount('#app');
+window.vm = vm;
